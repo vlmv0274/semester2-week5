98846b1082b83d2c8a9d2e89e3b3994ff676ac25
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-13 01:43:58 +0000

    Update task2.c

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -14,13 +14,31 @@ int main(void){
 	char hex[9];
 	
 	printf("Enter a hexadecimal:");
+	scanf("%8s", hex);
 
-	// if input contains invalid hex digit
-	// printf("Error: Invalid Hexadecimal\n");
-	
+	for (int i = 0; hex[i] != '\0'; i++) {
+        char hexletter = hex[i];
+        int value;
+		
+		if (hexletter >= '0' && hexletter <= '9') {
+			value = hexletter - '0';
+		}
+		else if (hexletter >= 'A' && hexletter <= 'F') {
+			value = hexletter - 'A' + 10;
+		}
+		// if input contains invalid hex digit
+		// printf("Error: Invalid Hexadecimal\n");
+		else {
+			printf("Error: Invalid Hexadecimal\n");
+			return 0;
+		}
+		decimal = decimal * 16 + value;
+	}
+
 	// print the decimal result
 	printf("decimal:%ld\n", decimal);
 	
 	return 0;
 
 }
+
